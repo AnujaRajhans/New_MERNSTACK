74fd4a5f8b05dbabc2e98074b9e752538c337bdb
AnujaRajhans <rajhansanuja3@gmail.com>
2024-07-22 12:33:24 +0530

    hello

--- pagination_proj/app.js
+++ pagination_proj/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
-const mysql= require('mysql');
-const path= require('path');
-const bodyparser= require('body-parser');
-PORT = 7000;
-app.use(bodyparser.urlencoded({extended: true}));
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
-})
-const createConnection = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '123',
-        database: 'demo'
-    }
-)
+const mysql = require('mysql');
+const path = require('path');
+const bodyParser = require('body-parser'); // Corrected the module name to 'body-parser'
+const PORT = 5011;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve your static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'demo'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('database connected');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
