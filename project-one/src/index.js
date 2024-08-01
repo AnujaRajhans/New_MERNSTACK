eb66e5039e8f01a70bb45aa71db1898a90e67fa7
AnujaRajhans <rajhansanuja3@gmail.com>
2024-08-01 12:32:00 +0530

    hello

---
+++ project-one/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const h1Element = React.createElement('h1',{className: 'demo1'},'hello ');
+console.log(h1Element);
+
+function createDivElement(){
+  return React.createElement('div',{},'hello');
+}
+console.log(createDivElement());
+
+
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
